82b7f6615fce27dcc70a5de5f4e113cfafb4f44a
Naveen Venkat <nav.naveenvenkat@gmail.com>
2017-01-27 15:17:07 +0000

    added list.h

---
+++ list.h
@@ -0,0 +1,254 @@
+#ifndef _GLIBCXX_IOSTREAM
+#include <iostream>
+#endif
+
+#include <stdlib.h>
+
+using namespace std;
+
+template <typename T> class Node
+{
+	public:
+		T data;
+		Node <T> * next;
+
+		Node<T>(T d)
+		{
+			data = d;
+			next = NULL;
+		}
+};
+
+#define TNode Node <T>
+
+template <typename T> class List
+{
+
+	private:
+		TNode * head;
+		unsigned long size;
+
+	public:
+
+		List <T> ()
+		{
+			head = NULL;
+			size = 0;
+		}
+
+		TNode * getNode(unsigned long pos)
+		{
+			if(head == NULL)
+			{
+				cout << endl << "Exception.List.getNode(): head is NULL" << endl;
+				system("exit");
+			}
+			else if(pos < 0 || pos > size-1)
+			{
+				cout << endl << "Exception.List.getNode(): index out of bounds" << endl;
+				system("exit");
+			}
+			else
+			{
+				TNode* mov = head;
+				for(int i=0;i<pos; i++)
+				{
+					mov = mov->next;
+				}
+				return mov;
+			}
+		}
+
+		TNode * getLast()
+		{
+			if(head == NULL)
+			{
+				return head;
+			}
+			else
+			{
+				TNode *mov = head;
+				while(mov->next!=NULL)
+				{
+					mov = mov->next;
+				}
+				return mov;
+			}
+		}
+
+		T getData(unsigned long pos)
+		{
+			if(head == NULL)
+			{
+				cout << endl << "Exception.List.get(): head is NULL" << endl;
+				system("exit");
+			}
+			else if(pos < 0 || pos > size-1)
+			{
+				cout << endl << "Exception.List.get(): index out of bounds" << endl;
+				system("exit");
+			}
+			else
+			{
+				TNode* mov = head;
+				for(int i=0;i<pos; i++)
+				{
+					mov = mov->next;
+				}
+				return mov->data;
+			}
+		}
+
+		T operator[] (unsigned long pos)
+		{
+			if(head == NULL)
+			{
+				cout << endl << "Exception.List.get(): head is NULL" << endl;
+				system("exit");
+			}
+			else if(pos < 0 || pos > size-1)
+			{
+				cout << endl << "Exception.List.get(): index out of bounds" << endl;
+				system("exit");
+			}
+			else
+			{
+				TNode* mov = head;
+				for(int i=0;i<pos; i++)
+				{
+					mov = mov->next;
+				}
+				return mov->data;
+			}
+		}
+
+		void add(T node, unsigned long pos)
+		{
+			if(pos < 0 || pos > size)
+			{
+				cout << endl << "Exception.List.add(): index out of bounds" << endl;
+				system("exit");
+			}
+			else
+			{
+				if (pos==0)
+				{
+					TNode * temp = new TNode(node);
+					temp -> next = head;
+					head = temp;
+				}
+				else
+				{
+					TNode * temp = getNode(pos-1);
+					TNode * addNode = new TNode(node);
+					addNode -> next = temp -> next;
+					temp -> next = addNode;
+				}
+				size++;
+			}
+		}
+
+		void add(T node)
+		{
+			TNode * temp = new TNode(node);
+			TNode * last = getLast();
+			if(last == NULL)
+			{
+				head = temp;
+			}
+			else
+			{
+				last -> next = temp;
+			}
+			size++;
+
+			// alternative code
+			/*	
+				add(node, size);
+			*/
+		}
+
+		void remove(unsigned long pos)
+		{
+			if(head == NULL)
+			{
+				cout << endl << "Exception.List.remove(): head is NULL" << endl;
+				system("exit");
+			}
+			else if(pos < 0 || pos > size-1)
+			{
+				cout << endl << "Exception.List.remove(): index out of bounds" << endl;
+				system("exit");
+			}
+			else
+			{
+				if(pos == 0)
+				{
+					TNode * temp= head;
+					head = head->next;
+					delete temp;
+				}
+				else
+				{
+					TNode * mov = head;
+					for(int i=1;i<pos; i++)
+					{
+						mov = mov->next;
+					}
+					TNode * temp = mov->next;
+					mov -> next = temp -> next;
+					delete temp;
+				}
+			}
+
+			size--;
+		}
+
+		void swap(unsigned long pos1, unsigned long pos2)
+		{
+			if( (pos1<0 || pos1>size-1) )
+			{
+				cout << endl << "Exception.List.swap(): first node index out of bounds" << endl;
+				system("exit");
+				return;
+			}
+			else if( (pos2<0 || pos2>size-1) )
+			{
+				cout << endl << "Exception.List.swap(): second node index out of bounds" << endl;
+				system("exit");
+				return;
+			}
+			else
+			{
+				T temp = getData(pos1);
+				getNode(pos1) -> data = getNode(pos2) -> data;
+				getNode(pos2) -> data = temp;
+			}
+		}
+
+		unsigned long getSize()
+		{
+			return size;
+		}
+
+		TNode * getHead()
+		{
+			return head;
+		}
+
+		void make(unsigned long number)
+		{
+			
+		}
+
+		/* 	
+			Utility Functions
+			- These need to be defined by the user.
+			- These use a function called 'compare' for comparing two nodes.
+			- Define a function called compare in class T.
+		*/
+
+		void sort();
+		T getMax();
+		T getMin();
+};
