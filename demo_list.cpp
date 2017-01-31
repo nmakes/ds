86a8a1db2c237d8fc365c468f05381626faed066
Naveen Venkat <nav.naveenvenkat@gmail.com>
2017-01-31 18:57:39 +0000

    improved demo presentation, added build demo

--- demo_list.cpp
+++ demo_list.cpp
@@ -8,11 +8,31 @@ using namespace std;
 int main()
 {
 	List <int> list;
-	cout << list.getSize() << endl;
+
+	cout << "\n-- initial --\n";
+	cout << "List size: " << list.getSize() << endl;
+
 	list.add(15);
-	list.add(5616);
+	list.add(13);
 	list.add(516,1);
-	cout << list.getSize() << endl;
-	cout << list[0] << "    " << list[1] << "    " << list[2];
+	cout << "\n-- adding three elements --\n";
+	cout << "List: ";
+	for(int i=0; i<=list.end(); i++)
+	{
+		cout << list.getData(i) << " ";
+	}
+	cout << endl;
+	cout << "New list size: " << list.getSize() << endl;
+
+	list.build(3);
+	cout << "\n-- after using build --\n";
+	cout << "List: ";
+	for(int i=0; i<=list.end(); i++)
+	{
+		cout << list.getData(i) << " ";
+	}
+	cout << endl;
+	cout << "New list size: " << list.getSize() << endl;
+
 	return 0;
 }
